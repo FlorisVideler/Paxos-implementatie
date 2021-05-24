4b20e6cfc4e3085ca07db818a78bd65ed050a027
Niels <nbijl@petersplanet.nl>
2021-05-24 13:08:29 +0200

    input examples added

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from classes.simulation import Simulation
 
 if __name__ == '__main__':
-    # Starts the simulation with input from 'input.txt'.
-    sim = Simulation('input.txt')
-    sim.start()
+    # Runs all the simulation examples.
+    inputs = ['input/example1.txt', 'input/example2.txt', 'input/example3.txt']  # input files
+
+    for inp in inputs:
+        print('EXAMPLE:', inp, '\n')
+        sim = Simulation(inp)  # create simulation with input example
+        sim.start()
+        print('\n\n')
